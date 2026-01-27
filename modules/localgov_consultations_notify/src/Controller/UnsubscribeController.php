cd97b707eadcd38a3d964603df98918f51c80f26
Parth Sinha <parth.sinha@webcurl.co.uk>
2026-01-27 09:55:15 +0000

    updated the fuction and used dependency injection

--- modules/localgov_consultations_notify/src/Controller/UnsubscribeController.php
+++ modules/localgov_consultations_notify/src/Controller/UnsubscribeController.php
@@ -3,8 +3,11 @@
 namespace Drupal\localgov_consultations_notify\Controller;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Access\AccessResultInterface;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Access controller for the unsubscribe form.
@@ -12,31 +15,55 @@ use Drupal\Core\Session\AccountInterface;
 class UnsubscribeController extends ControllerBase {
 
   /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected RouteMatchInterface $routeMatch;
+
+  /**
+   * Constructs an UnsubscribeController object.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The current route match.
+   */
+  public function __construct(RouteMatchInterface $route_match) {
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('current_route_match'),
+    );
+  }
+
+  /**
    * Check that the hash in the email matches the subscription hash.
    *
    * @param \Drupal\Core\Session\AccountInterface $account
-   *  The current user account
+   *   The current user account.
    *
    * @return \Drupal\Core\Access\AccessResultInterface
-   *  The access result.
+   *   The access result.
    */
-  public function access(AccountInterface $account) {
-    /** @var \Drupal\mailing_list\Entity\Subscription $subscription */
-    $subscription = \Drupal::routeMatch()->getParameter('mailing_list_subscription');
+  public function access(AccountInterface $account): AccessResultInterface {
+    /** @var \Drupal\mailing_list\Entity\Subscription|null $subscription */
+    $subscription = $this->routeMatch->getParameter('mailing_list_subscription');
     if (!$subscription) {
       return AccessResult::forbidden();
     }
 
     $hash = $subscription->getAccessHash();
-
-    $token = \Drupal::routeMatch()->getParameter('token');
+    $token = $this->routeMatch->getParameter('token');
 
     if ($hash === $token) {
       return AccessResult::allowed();
     }
-    else {
-      return AccessResult::forbidden();
-    }
+
+    return AccessResult::forbidden();
   }
 
 }
--- modules/localgov_consultations_notify/src/Form/UnsubscribeForm.php
+++ modules/localgov_consultations_notify/src/Form/UnsubscribeForm.php
@@ -2,24 +2,64 @@
 
 namespace Drupal\localgov_consultations_notify\Form;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\ConfirmFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\Core\Url;
-use Drupal\node\Entity\Node;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Removes a user's mailing_list_subscription.
- * Via special URL access.
- * So they don't have to go via the dodgy
+ *
+ * Via special URL access so they don't have to go via the dodgy
  * mailing_list interface.
  */
 class UnsubscribeForm extends ConfirmFormBase {
 
   /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected RouteMatchInterface $routeMatch;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * Constructs an UnsubscribeForm object.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The current route match.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(RouteMatchInterface $route_match, EntityTypeManagerInterface $entity_type_manager) {
+    $this->routeMatch = $route_match;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
    * {@inheritdoc}
    */
-  public function getQuestion() {
-    $subscription = \Drupal::routeMatch()->getParameter('mailing_list_subscription');
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('current_route_match'),
+      $container->get('entity_type.manager'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getQuestion(): TranslatableMarkup {
+    $subscription = $this->routeMatch->getParameter('mailing_list_subscription');
 
     $label = 'these emails';
 
@@ -28,7 +68,7 @@ class UnsubscribeForm extends ConfirmFormBase {
         $consultation_id = $subscription->get('field_node')->getValue()[0]['target_id'];
         // SHOULD be set, but just in case...
         if ($consultation_id) {
-          $consultation = Node::load($consultation_id);
+          $consultation = $this->entityTypeManager->getStorage('node')->load($consultation_id);
           $label = 'updates on consultation: ' . $consultation->label();
         }
         break;
@@ -44,22 +84,22 @@ class UnsubscribeForm extends ConfirmFormBase {
   /**
    * {@inheritdoc}
    */
-  public function getCancelUrl() {
+  public function getCancelUrl(): Url {
     return new Url('<front>');
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getConfirmText() {
+  public function getConfirmText(): TranslatableMarkup {
     return $this->t('Unsubscribe');
   }
 
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    $subscription = \Drupal::routeMatch()->getParameter('mailing_list_subscription');
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $subscription = $this->routeMatch->getParameter('mailing_list_subscription');
     $subscription->delete();
     $this->messenger()->addMessage($this->t('You have been unsubscribed!'));
 
@@ -70,9 +110,9 @@ class UnsubscribeForm extends ConfirmFormBase {
    * Returns a unique string identifying the form.
    *
    * @return string
-   *  The form ID
+   *   The form ID.
    */
-  public function getFormId() {
+  public function getFormId(): string {
     return 'localgov_consultations_unsubscribe';
   }
 
